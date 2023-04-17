768596ce5f835e82d0bb12fefeb5a0ac27c3cb57
theNickola <kurilchik.nv@gmail.com>
2023-04-17 22:52:16 +0300

    feat: remove

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -48,7 +48,19 @@ class BinarySearchTree {
   }
 
   has(data) {//throw new NotImplementedError('Not implemented');
-    
+    function searchWithin(node, value) {
+      if (!node) 
+        return false;
+
+      if (node.data === value) 
+        return true;
+
+      return value < node.data ?
+        searchWithin(node.left, value):
+        searchWithin(node.right, value);
+    }
+
+    return searchWithin(this.root, data);
   }
 
   find(/* data */) {
@@ -56,9 +68,46 @@ class BinarySearchTree {
     // remove line with error and write your code here
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {//throw new NotImplementedError('Not implemented');
+    function removeNode(node, value) {
+      if (!node) 
+        return null;
+
+      if (value < node.value) {
+        node.left = removeNode(node.left, value);
+        return node;
+      } 
+      else if (value > node.value) {
+        node.right = removeNode(node.right, value);
+        return node;
+      } 
+      else {
+        if (!node.left && !node.right) 
+          return null;
+
+        if (!node.left) {
+          node = node.right;
+          return node;
+        }
+
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+
+        let minFromRight = node.right;
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left;
+        }
+
+        node.value = minFromRight.value;
+        node.right = removeNode(node.right, minFromRight.value);
+
+        return node;
+      }
+    }
+
+    this.root = removeNode(this.root, data);
   }
 
   min() {
